c80aabb8ae01f7ddc8233c79730f8530d98112a7
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2019-04-01 07:59:13 +0300

    Make url function dynamic

--- es6/modules/url.js
+++ es6/modules/url.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 
-const appHost = $('meta[name="appHost"]')[0].content;
-const url = (route) => `${appHost}${route}`;
+const url = (route, name = 'appRoot') => {
+  const prefix = $(`meta[name="${name}"]`)[0].content;
+  return `${prefix}${route}`;
+}
 
 export default url;
