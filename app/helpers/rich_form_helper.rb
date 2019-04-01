6c59c361906071e8df30312a3adf462e341a83a9
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2019-04-01 08:24:23 +0300

    Use default params in rich_form_for helper

--- app/helpers/rich_form_helper.rb
+++ app/helpers/rich_form_helper.rb
@@ -1,5 +1,9 @@
 module RichFormHelper
   def rich_form_for(record, options = {}, &block)
+    options[:authenticity_token] = true unless options.key?(:authenticity_token)
+    options[:remote] = true unless options.key?(:remote)
+    options[:html] = {} unless options.key?(:html)
+    options[:html][:id] = 'form' unless options[:html].key?(:id)
     form_for(record, options.merge(builder: RichFormBuilder), &block)
   end
 end
