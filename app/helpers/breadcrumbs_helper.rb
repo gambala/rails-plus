6bef8add462c8473c49f5353e9e7b06d2659d22d
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2019-04-01 08:20:45 +0300

    Show only active breadcrumbs with container link

--- app/helpers/breadcrumbs_helper.rb
+++ app/helpers/breadcrumbs_helper.rb
@@ -1,14 +1,14 @@
 module BreadcrumbsHelper
   def render_breadcrumbs
     return if breadcrumbs.empty?
+    return if breadcrumbs.size == 1
 
     content_tag :div, class: 'breadcrumb-list' do
-      breadcrumbs.each_with_index do |breadcrumb, index|
-        if index + 1 == breadcrumbs.size
-          concat content_tag :div, breadcrumb[:label], class: 'breadcrumb-item'
-        else
-          concat link_to breadcrumb[:label], breadcrumb[:route], class: 'breadcrumb-item'
-        end
+      breadcrumb = breadcrumbs[-2]
+      concat link_to '', breadcrumb[:route], class: 'breadcrumb-list__link' if breadcrumb.present?
+
+      breadcrumbs[0..-2].each do |breadcrumb|
+        concat link_to breadcrumb[:label], breadcrumb[:route], class: 'breadcrumb-item'
       end
     end
   end
