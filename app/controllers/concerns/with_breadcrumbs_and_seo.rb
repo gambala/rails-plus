ddd8648334c69e7b2a50494a83cceafb66fca4cf
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2019-07-05 21:29:44 +0300

    Use first breadcrumb in title parametrically

--- app/controllers/concerns/with_breadcrumbs_and_seo.rb
+++ app/controllers/concerns/with_breadcrumbs_and_seo.rb
@@ -6,19 +6,20 @@ module WithBreadcrumbsAndSeo
       breadcrumbs[-1][:label]
     end
 
-    def title_from_breadcrumbs
-      breadcrumbs[1..-1].reverse.map { |b| b[:label] }.join(' - ')
+    def title_from_breadcrumbs(with_first: false)
+      starts_from = with_first ? 0 : 1
+      breadcrumbs[starts_from..-1].reverse.map { |b| b[:label] }.join(' - ')
     end
 
     def site_title
       t('site.title')
     end
 
-    def set_seo_tags_with_breadcrumbs(force: false)
+    def set_seo_tags_with_breadcrumbs(with_first: false)
       return if breadcrumbs.empty?
-      return if breadcrumbs.size == 1 && force == false
+      return if breadcrumbs.size == 1 && with_first == false
       h1 h1_from_breadcrumbs || site_title
-      title [title_from_breadcrumbs, site_title].compact.join(' | ')
+      title [title_from_breadcrumbs(with_first), site_title].compact.join(' | ')
     end
   end
 end
