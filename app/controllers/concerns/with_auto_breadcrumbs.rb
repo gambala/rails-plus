8943497c1876ace0b51ebd6df74cc285c024ac3d
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2019-07-09 13:28:58 +0300

    Use model param instead of resource in auto_breadcrumbs concern

--- app/controllers/concerns/with_auto_breadcrumbs.rb
+++ app/controllers/concerns/with_auto_breadcrumbs.rb
@@ -2,18 +2,18 @@ module WithAutoBreadcrumbs
   extend ActiveSupport::Concern
 
   included do
-    def auto_breadcrumbs(resource, only: %i(index new show edit delete), prefix: [])
+    def auto_breadcrumbs(model, only: %i(index new show edit delete), prefix: [])
       only.each do |action|
         case action
         when :create, :update, :destroy
           next
         when :index
           # add_breadcrumb User, [:prefix, :users]
-          add_breadcrumb resource, prefix + [resource.model_name.route_key]
+          add_breadcrumb model, prefix + [model.model_name.route_key]
         when :show
           # add_breadcrumb user.to_s, [:prefix, user] if user.try(:persisted?)
-          method_name = if self.class.private_method_defined?(resource.model_name.element)
-                          resource.model_name.element
+          method_name = if self.class.private_method_defined?(model.model_name.element)
+                          model.model_name.element
                         else
                           'record'
                         end
@@ -30,10 +30,10 @@ module WithAutoBreadcrumbs
           next unless actions.include?(params[:action].to_sym)
 
           if action == :new
-            route = resource.model_name.singular_route_key
+            route = model.model_name.singular_route_key
           else
             method_name = 'record' if self.class.private_method_defined?('record')
-            method_name ||= resource.model_name.element
+            method_name ||= model.model_name.element
             record = self.send(method_name)
             route = record
           end
