1a5b71b7e746db1e05a691866e06ae2b5f00d3a4
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2019-08-16 01:03:09 +0300

    Show year for future dates in smart_datetime helper

--- app/helpers/datetime_helper.rb
+++ app/helpers/datetime_helper.rb
@@ -1,12 +1,14 @@
 module DatetimeHelper
-  def smart_datetime(datetime, without_time: false, with_time: true)
+  def smart_datetime(datetime, without_time: false, with_time: true, force_with_year: false)
     return if datetime.blank?
     template = if datetime > Time.zone.now.midnight && datetime < Time.zone.now.end_of_day
                  '%H:%M'
-               elsif datetime > Time.zone.now.beginning_of_year
-                 without_time ? '%d %b' : '%d %b %H:%M'
-               else
+               elsif force_with_year || \
+                     datetime < Time.zone.now.beginning_of_year || \
+                     datetime > Time.zone.now.end_of_year
                  without_time ? '%d %b %Y' : '%d %b %Y %H:%M'
+               else
+                 without_time ? '%d %b' : '%d %b %H:%M'
                end
     l datetime, format: template
   end
