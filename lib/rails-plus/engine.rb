0b36e8c11de83e0b6bcda443e79afebca429965b
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2020-05-24 22:45:41 +0300

    Do not include RoutesHelper automatically because of bugs in zeitwerk

--- lib/rails-plus/engine.rb
+++ lib/rails-plus/engine.rb
@@ -1,9 +1,4 @@
 module RailsPlus
   class Engine < ::Rails::Engine
-    initializer 'rails-plus.include_view_helpers' do |app|
-      ActiveSupport.on_load :action_view do
-        include RoutesHelper
-      end
-    end
   end
 end
